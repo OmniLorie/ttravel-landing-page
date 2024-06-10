15cc6deae408e66973fa660f6402761163fdfc52
LorettaOsibe <losibe27@gmail.com>
2024-06-10 10:02:57 +0000

    first commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById('btn-btn')
+btn.onclick = function(){
+   alert('Wait, more locations coming soon')
+}
+const btn2 = document.getElementById('btn-header')
+btn2.onclick = function(){
+   alert('The World of Travel')
+}
